3083b12c418ed16fbb68f96952d196ddbff34f6a
nick <niko456gt@gmail.com>
2024-08-04 14:19:09 -0700

    update sqlquery to create/delete dbs

--- Restore-SQL.ps1
+++ Restore-SQL.ps1
@@ -1,13 +1,29 @@
 #Leal Barrera, Nicolas Student ID: 011150100
 
-
-$ServerName = "SRV19-PRIMARY\SQLEXPRESS"
-$Connection = New-Object System.Data.SqlClient.SqlConnection("Server=$ServerName;Integrated Security=True;")
-$Connection.Open()
-
-Invoke-Sqlcmd -ServerInstance $ServerName -Query "SELECT name FROM sys.databases"
+Set-Location  SQLSERVER:\\\SQL\SRV19-PRIMARY\SQLEXPRESS
 
 
-$Connection.Close()
+$MYDB = (Get-SqlDatabase -ServerInstance "SRV19-PRIMARY\SQLEXPRESS" | Select-Object Name)
+$Target = "ClientDB"
+$Found = $MYDB | Where-Object {$_.Name -eq $Target}
 
+if ($Found) {
+    try {
+        $DropDB = Get-SqlDatabase -ServerInstance "SRV19-PRIMARY\SQLEXPRESS" -Name $Target
+        $DropDB.Drop()
+        Write-Host "$Target Deleted from server"
+    }
+    catch [System.Exception]{
+        Write-Host "Something went wrong $($_.Exception.Message)"
+    }
+    catch {
+        Write-Host "Somethign went wrong: $_ "
+    }
+}else {
+    Write-Host "$Target does not exist"
+}
+
+Write-Host "Creating DB $Target"
+
+Invoke-Sqlcmd -Server "SRV19-PRIMARY\SQLEXPRESS"   -Query "CREATE DATABASE ClientDB"
 
